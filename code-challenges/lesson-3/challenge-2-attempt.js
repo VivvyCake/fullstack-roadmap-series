097facbfd14cbe1bd417d6528b42e8a271df1012
VivianSamuels <vsamuels@umd.edu>
2023-08-01 17:29:57 -0400

    challenege 2 attempt 1

---
+++ code-challenges/lesson-3/challenge-2-attempt.js
@@ -0,0 +1,16 @@
+/*
+There are three things wrong with this code. 
+Find them and explain why they are wrong. 
+*/
+
+/* 
+const some Number = 20;
+someNumber = 50
+*/
+
+/* 
+1 a const variable cannot be reassigned
+2 "some Number" and "someNumber" are not the same. 
+  There should be no spaces in a variable name
+3 JS requires ; at the end of each line
+*/
