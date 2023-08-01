59cd6f84a6bf0c15fb159376bedfe0d4dd5d52b4
VivianSamuels <vsamuels@umd.edu>
2023-08-01 17:41:54 -0400

    challenge 5 attempt 1

---
+++ code-challenges/lesson-3/challenege-5-attempt.js
@@ -0,0 +1,15 @@
+/* 
+Why does this code not work? Edit until it works.
+*/
+
+/*
+const myArray = [20, 30, 40];
+
+console.log(myArray[3]);
+*/
+
+//the array has a length of 3, but on has indexes 0 - 2
+//therfore an index of 3 is invalid
+const myArray = [20, 30, 40];
+
+console.log(myArray[2]);
