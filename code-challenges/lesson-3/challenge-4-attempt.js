6fda518206c26199b6656b9553c7a101637099a1
VivianSamuels <vsamuels@umd.edu>
2023-08-01 17:36:12 -0400

    challenege 4 attempt 1

---
+++ code-challenges/lesson-3/challenge-4-attempt.js
@@ -0,0 +1,23 @@
+/* 
+Why is this code invalid? Edit this until it is valid.
+*/
+
+/*
+const objectVariable = {
+    property1: 'i am property 1';
+    property2: 'i am property 2';
+    property3: [20, 30, 40];
+  };
+  
+  console.log(objectVariable.property3[2]);
+*/
+
+//replace semicolons with commas
+
+  const objectVariable = {
+    property1: 'i am property 1',
+    property2: 'i am property 2',
+    property3: [20, 30, 40]
+  };
+  
+  console.log(objectVariable.property3[2]);
